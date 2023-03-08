b52542a58aa77c713003da4c5ffc23c2a1c69d32
drashti162002 <drashtikathrotiya.rao@gmail.com>
2023-03-08 21:32:01 +0530

    last commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
- 
 function addGrid(){
     var columns =prompt("Enter no of Column :");
     var rows =prompt("Enter No of Row :"); 
@@ -8,12 +7,10 @@ function addGrid(){
     for (var i = 1; i <= columns; i++) {
         var column = document.createElement('div'); 
         column.className = 'column';
-        column.textContent = i; 
 
         for (var j = 1; j <= rows; j++) {
             var row = document.createElement('div'); 
             row.className = 'row';
-            row.textContent = j;
             column.appendChild(row); 
         }
         gridBox.appendChild(column); 
@@ -22,56 +19,6 @@ function addGrid(){
     document.body.appendChild(gridBox);
     addColor();
  }
-
-// let colorInput = document.querySelector('#color');   
-
-
-// function addColor(){
-// //let gridBox = document.querySelectorAll('.gridBox');
-// let row = document.querySelector(".gridBox .row");
-// row.forEach(row =>{
-//     row.addEventListener('mouseover', () =>{
-//         console.log("color");
-//         row.style.backgroundColor = "black";
-//     })
-// })
-// }
-
-
-
-
-
-// function addColor(){
-//     let gridBox = document.querySelectorAll('.gridBox');
-//     // let row = document.querySelector(".gridBox .row");
-//     gridBox.forEach(gridBox =>{
-//         gridBox.addEventListener('mouseover', () =>{
-//             console.log("color");
-//             gridBox.style.backgroundColor = "black";
-//         })
-//     })
-// }
-
-// function addColor(){
-//     let div = document.querySelectorAll('div');
-//     // let row = document.querySelector(".gridBox .row");
-//     div.forEach(div =>{
-//         div.addEventListener('mouseover', () =>{
-//             console.log("color");
-//             div.style.backgroundColor = "black";
-//         })
-//     })
-// }
-
-// function addColor(){
-//     let gridElements = document.querySelectorAll('.gridBox .column .row');
-//     gridElements.forEach(div =>{
-//         div.addEventListener('mouseover', () =>{
-//             console.log("color");
-//             div.style.backgroundColor = "pink";
-//         })
-//     })
-// }                   
 
 function addColor(){
     let  colorInput = document.querySelector("#color");
